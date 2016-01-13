f78dbe8fcb57270fe3205641d8768790e8f4a2f4
Marcelo Rusconi <marcelo.rusconi@avantrip.com>
2016-01-13 16:54:52 -0300

    termine campos.json

--- class/test.php
+++ class/test.php
@@ -1,10 +1,15 @@
-<h1>TKB</h1>
 <?php
-    $json_file = file_get_contents("campos.json");
-    $campos = json_decode($json_file);
-
-    foreach($campos->data as $campo){
-        var_dump($campo->numero);
-        echo '<br />';
+    $json_file = file_get_contents("../campos.csv");
+    $array = array_map("str_getcsv", explode("\n", $json_file));
+    $json = json_decode(json_encode($array));
+    $keys = array_shift($json);
+    foreach ($json as $campo){
+        foreach ($campo as $key => $data){
+            $tmp[$keys[$key]] =$data;
+        }
+        $campos[] = $tmp;
     }
+echo "<pre>";
+print_r(json_encode($campos));
+echo "</pre>";
 ?>
